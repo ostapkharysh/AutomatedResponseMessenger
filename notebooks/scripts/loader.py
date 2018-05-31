c200ce9af2a1528b8310f95ad72d0b09034ea44c
olha.bakay <Olhabak@gmail.com>
2018-05-31 17:03:18 +0300

    Added next word prediction using Markov Model

--- notebooks/scripts/loader.py
+++ notebooks/scripts/loader.py
@@ -2,11 +2,13 @@ import pandas as pd
 import gzip
 from collections import Counter
 
+
 def load_data(path):
     df = []
     for line in gzip.open(path, 'rb'):
         df.append(eval(line))
     return pd.DataFrame.from_dict(df)
+
 
 if __name__ == "__main__":
     df = load_data('qa_Appliances.json.gz')
